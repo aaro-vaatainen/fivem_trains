10e5cc7e2109f9a02347cbab802034b501c2895d
Aaro Vaatainen <aaro.maili.vaatainen@gmail.com>
2025-02-02 13:28:26 +0200

    Revert "0.0.2"
    This reverts commit 6a567c8ab481fad3c2ce9aed8b3cf346ebce0a0b.

--- client.lua
+++ client.lua
@@ -1,55 +1,17 @@
--- Function to read config file
-function LoadConfig()
-    local config = {}
-    local file = io.open("config.cfg", "r")
-
-    if file then
-        for line in file:lines() do
-            local key, value = line:match("^(.-)=(.-)$")
-            if key and value then
-                -- Trim spaces
-                key = key:match("^%s*(.-)%s*$")
-                value = value:match("^%s*(.-)%s*$")
-
-                -- Convert numeric values if needed
-                if value:match("^%d+$") then
-                    value = tonumber(value)
-                elseif value:match("^%d+,%d*") then
-                    local list = {}
-                    for num in value:gmatch("%d+") do
-                        table.insert(list, tonumber(num))
-                    end
-                    value = list
-                end
-
-                config[key] = value
-            end
-        end
-        file:close()
-    else
-        print("[Train Script] Error loading config.cfg! Using defaults.")
-        config["train_tracks"] = {0, 1, 3, 4, 5, 6, 7, 8, 10}
-        config["freight_train_frequency"] = 30000
-        config["metro_train_frequency"] = 30000
-    end
-
-    return config
-end
-
 Citizen.CreateThread(function()
-    local config = LoadConfig()
+    -- Activate train tracks
+    local trainTracks = {0, 1, 3, 4, 5, 6, 7, 8, 10} -- List of train track IDs to enable
 
-    -- Activate train tracks from config
-    for _, track in ipairs(config["train_tracks"]) do
+    for _, track in ipairs(trainTracks) do
         SwitchTrainTrack(track, true)
     end
 
-    -- Set train spawn frequency from config
-    SetTrainTrackSpawnFrequency(0, config["freight_train_frequency"]) -- Freight train
-    SetTrainTrackSpawnFrequency(3, config["metro_train_frequency"]) -- Metro train
+    -- Set spawn frequency
+    SetTrainTrackSpawnFrequency(0, 120000) -- Freight train spawn frequency
+    SetTrainTrackSpawnFrequency(3, 120000) -- Metro train spawn frequency
 
     -- Enable random trains
     SetRandomTrains(true)
 
-    print("[Train Script] Junat ladattu. Frekvenssi: " .. config["freight_train_frequency"] .. "ms (Freight), " .. config["metro_train_frequency"] .. "ms (Metro)")
+    print("[Juna Scripti] Junat ladattu onnistuneesti.")
 end)
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,4 @@
 fx_version 'cerulean'
 game 'gta5'
 
-client_scripts {
-    'config.cfg',
-    'train_control.lua'
-}
+client_script 'client.lua'
